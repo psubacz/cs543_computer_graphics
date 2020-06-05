854930e8e995bdf9f18c5fa8d9d2a0b891723249
Peter Subacz <psubacz@gmail.com>
2020-06-05 16:38:20 -0400

    working drawing

--- project 1/project_1.js
+++ project 1/project_1.js
@@ -6,7 +6,6 @@
  * @author Peter Subacz
  */
 
-
 function init(){
 	//display the file upload button
 	document.getElementById('image-file').style.display = 'block';
@@ -15,7 +14,6 @@ function init(){
 	//display the default color being displayed mode 
 	document.getElementById('colorMode').innerHTML = 'Color: Black'
 }
-
 
 function parse_text_file(rawText){
 	/*
@@ -68,20 +66,47 @@ async function draw_mode()
 	// await ;
 }
 
-// async function asyncCall() {
-// 	console.log('calling');
-// 	const result = await resolveAfter2Seconds();
-// 	console.log(result);
-// 	// expected output: 'resolved'
-//   }
-
- function file_mode(gl,polygonsList = null)
+ function file_mode(gl,polygonList)
 {
-	document.getElementById("pageMode").innerHTML = 'File Mode';	//Display the mode
-	document.getElementById('image-file').style.display = 'block';	//Display the button
-	// 
-	var vBuffer = gl.createBuffer();
-	
+	if (polygonList == null){
+		document.getElementById("fileContent").innerHTML = 'Upload a file to draw.';
+
+	}else{
+		// Set clear color
+		gl.clearColor(1.0, 1.0, 1.0, 1.0);
+		// Clear <canvas> by clearing the color buffer
+		gl.clear(gl.COLOR_BUFFER_BIT);
+		console.log(polygonList.size)
+		for(i=1;i<polygonList.length;i++){
+			var vBuffer = gl.createBuffer();
+			gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+			gl.bufferData(gl.ARRAY_BUFFER, flatten(polygonList[i]), gl.STATIC_DRAW);
+
+			var vPosition = gl.getAttribLocation(program, "vPosition");
+			gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
+			gl.enableVertexAttribArray(vPosition);
+
+			var offsetLoc = gl.getUniformLocation(program, "vPointSize");
+			gl.uniform1f(offsetLoc, 1.0);
+
+			/*** COLOR DATA ***/
+			var colors = [];
+			for(ii=1;ii<polygonList[i].length+1;ii++){
+				colors.push(vec4(0.0, 0.0, 0.0, 1.0));
+			}
+
+			var cBuffer = gl.createBuffer();
+			gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+			gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+
+			var vColor = gl.getAttribLocation(program, "vColor");
+			gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
+			gl.enableVertexAttribArray(vColor);
+			
+			// Draw a point
+			gl.drawArrays(gl.LINES, 0, polygonList[i].length);
+		}
+	}
 }
 
 function change_color(colorIndex,colorList)
@@ -144,7 +169,6 @@ function main()
 	init();
 
 	var polygonsList = [];
-	var polygonsReady = false;
 	// Add the event listener to parse input file
 	document.getElementById('image-file').addEventListener('change', function() {
 		var fr = new FileReader();
@@ -152,7 +176,7 @@ function main()
 			polygonsList = parse_text_file(fr.result);
 			console.log('Jobs Done')
 			// var polygonsReady = true;
-			file_mode(polygonsList)
+			file_mode(gl,polygonsList)
 		}
 		fr.readAsText(this.files[0]);
 	}) 
@@ -182,80 +206,9 @@ function main()
 	//canvas is the window, and viewport is the viewing area within that window
 		//This tells WebGL the -1 +1 clip space maps to 0 <-> gl.canvas.width for x and 0 <-> gl.canvas.height for y
 	gl.viewport( 0, 0, canvas.width, canvas.height );
-	
-	/**********************************
-	* Points, Lines, and Fill
-	**********************************/
-	
-	/*** VERTEX DATA ***/
-	//Define the positions of our points
-	//Note how points are in a range from 0 to 1
-	var points = [];
-	points.push(vec4(-0.5, -0.5, 0.0, 1.0));
-	points.push(vec4(0.5, -0.5, 0.0, 1.0));
-	points.push(vec4(0.0, 0.5, 0.0, 1.0));
-	
 
-	//Create the buffer object
-	var vBuffer = gl.createBuffer();
-
-	//Bind the buffer object to a target
-	//The target tells WebGL what type of data the buffer object contains, 
-	//allowing it to deal with the contents correctly
-	//gl.ARRAY_BUFFER - specifies that the buffer object contains vertex data
-	gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-
-	//Allocate storage and write data to the buffer
-	//Write the data specified by the second parameter into the buffer object
-	//bound to the first parameter
-	//We use flatten because the data must be a single array of ints, uints, or floats (float32 or float64)
-	//This is a typed array, and we can't use push() or pop() with it
-	//
-	//The last parameter specifies a hint about how the program is going to use the data
-	//stored in the buffer object. This hint helps WebGL optimize performance but will not stop your
-	//program from working if you get it wrong.
-	//STATIC_DRAW - buffer object data will be specified once and used many times to draw shapes
-	//DYNAMIC_DRAW - buffer object data will be specified repeatedly and used many times to draw shapes
-	gl.bufferData(gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
-
-	//Get the location of the shader's vPosition attribute in the GPU's memory
-	var vPosition = gl.getAttribLocation(program, "vPosition");
-
-	//Specifies how shader should pull the data
-	//A hidden part of gl.vertexAttribPointer is that it binds the current ARRAY_BUFFER to the attribute.
-	//In other words now this attribute is bound to vColor. That means we're free to bind something else
-	//to the ARRAY_BUFFER bind point. The attribute will continue to use vPosition.
-	gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
-
-	//Turns the attribute on
-	gl.enableVertexAttribArray(vPosition);
-
-	//Specify the vertex size
-	var offsetLoc = gl.getUniformLocation(program, "vPointSize");
-	gl.uniform1f(offsetLoc, 10.0);
-
-	/*** COLOR DATA ***/
-	var colors = [];
-	colors.push(vec4(1.0, 0.0, 0.0, 1.0));
-	colors.push(vec4(0.0, 1.0, 0.0, 1.0));
-	colors.push(vec4(0.0, 0.0, 1.0, 1.0));
-
-	var cBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
-
-	var vColor = gl.getAttribLocation(program, "vColor");
-	gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
-	gl.enableVertexAttribArray(vColor);
-	
-	// Set clear color
-	gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
-	// Clear <canvas> by clearing the color buffer
-	gl.clear(gl.COLOR_BUFFER_BIT);
-	
-	// Draw a point
-	gl.drawArrays(gl.POINTS, 0, points.length);
+	//start off in filemode
+	file_mode(gl,null);
 
 	window.onkeypress = function(event)
 	{
@@ -274,7 +227,8 @@ function main()
 
 		case 'f':
 			//file mode
-			file_mode(gl)
+			document.getElementById("pageMode").innerHTML = 'File Mode';	//Display the mode
+			document.getElementById('image-file').style.display = 'block';	//Display the button
 			break;
 
 		case 'd':
@@ -287,5 +241,4 @@ function main()
 			colorIndex = change_color(colorIndex,colorList);
 		}
 	}
-	file_mode(gl);
 }
